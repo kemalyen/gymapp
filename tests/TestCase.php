88f551652c66cb8bc2e0a12925f81e91b2262eef
Kemal Yenilmez <kemal.yenilmez@exertis.co.uk>
2023-12-18 17:41:44 +0000

    sada

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,6 +5,7 @@ namespace Tests;
 use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
+use Spatie\Permission\Models\Role;
 
 abstract class TestCase extends BaseTestCase
 {
@@ -12,9 +13,14 @@ abstract class TestCase extends BaseTestCase
     use LazilyRefreshDatabase;
 
     protected function setUp(): void
-{
-    parent::setUp();
- 
-    $this->actingAs(User::factory()->create());
-}
+    {
+        parent::setUp();
+        Role::create(['name' => 'member']);
+        Role::create(['name' => 'admin']);
+        Role::create(['name' => 'trainer']);
+        Role::create(['name' => 'staff']);
+        Role::create(['name' => 'sales']);
+        Role::create(['name' => 'trial']);
+        $this->actingAs(User::factory()->admin()->create());
+    }
 }
